2a199788486a0b5b34e7acf8875d669f130aaf61
Matt <matthew.thompson.a@gmail.com>
2016-09-12 19:53:22 -0400

    Working Through Challenge Week3

--- Challenges/Week3/CodeLou.CSharp.Week3.Challenge/CodeLou.CSharp.Week3.Challenge/Appointments.cs
+++ Challenges/Week3/CodeLou.CSharp.Week3.Challenge/CodeLou.CSharp.Week3.Challenge/Appointments.cs
@@ -9,8 +9,6 @@ namespace CodeLou.CSharp.Week3.Challenge
     public class Appointments : CalendarItemBase
     {
         // Appointments need to be assigned a start date and time, an end date and time, and a location.
-        var myAppointment = new Appointments(); 
-
         public DateTime StartDateTime { get; set; }
         public DateTime EndDateTime { get; set; }
         public string Location { get; set; }
